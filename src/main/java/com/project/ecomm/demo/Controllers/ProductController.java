bc703894075a5e5095d60b63886b1540c194f6ff
ABHISHEK KRISHNA <ktmabhi007@gmail.com>
2025-07-20 20:26:52 +0530

    All Product Controller NullPointerException will be handled by handleNullPointerException

--- src/main/java/com/project/ecomm/demo/Controllers/ProductController.java
+++ src/main/java/com/project/ecomm/demo/Controllers/ProductController.java
@@ -1,9 +1,11 @@
 package com.project.ecomm.demo.Controllers;
 
 import com.project.ecomm.demo.Models.Product;
+import com.project.ecomm.demo.dtos.ExceptionDTO;
 import com.project.ecomm.demo.dtos.ProductResponseDTO;
 import com.project.ecomm.demo.service.FakeStoreProductService;
 import com.project.ecomm.demo.service.ProductService;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,5 +24,15 @@ public class ProductController {
     public ProductResponseDTO getProductById(@PathVariable("id") long id){
         Product product =  productService.getProductById(id);
         return ProductResponseDTO.from(product);
+    }
+
+//    Instead of exposing the internal package flow in postman
+//            we have beautified the error message now
+    @ExceptionHandler(NullPointerException.class)
+    public ExceptionDTO handleNullPointerException(){
+        ExceptionDTO exceptionDTO = new ExceptionDTO();
+        exceptionDTO.setStatus("Failure!!");
+        exceptionDTO.setMessage("Product Cannot Be Null!!");
+        return exceptionDTO;
     }
 }
---
+++ src/main/java/com/project/ecomm/demo/dtos/ExceptionDTO.java
@@ -0,0 +1,11 @@
+package com.project.ecomm.demo.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ExceptionDTO {
+    private String status;
+    private String message;
+}
