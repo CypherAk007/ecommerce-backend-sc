f2c36d46c2be3c521290de9740e0ddd3c93e2c26
ABHISHEK KRISHNA <ktmabhi007@gmail.com>
2025-07-20 21:25:39 +0530

    404 - Custom Global Product Exception.

--- src/main/java/com/project/ecomm/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/project/ecomm/demo/exceptions/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.project.ecomm.demo.exceptions;
 
 import com.project.ecomm.demo.dtos.ExceptionDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,11 +16,13 @@ public class GlobalExceptionHandler {
         return exceptionDTO;
     }
 
+    // Normally spring return 200 default for all - even for exceptions
+//    if you want custom status code then use ResponseEntity
     @ExceptionHandler(ProductNotFoundException.class)
-    public ExceptionDTO handleGlobalProductNotFoundException(Exception e){
+    public ResponseEntity<ExceptionDTO> handleGlobalProductNotFoundException(Exception e){
         ExceptionDTO exceptionDTO = new ExceptionDTO();
         exceptionDTO.setStatus("Failure");
         exceptionDTO.setMessage(e.getMessage());
-        return exceptionDTO;
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.NOT_FOUND);
     }
 }
